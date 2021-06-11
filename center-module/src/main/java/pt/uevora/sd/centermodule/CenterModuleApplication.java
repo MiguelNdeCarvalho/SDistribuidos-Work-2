c844e6ff8e500a1dac8a424be382333a59cd12e5
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-06-11 23:21:23 +0100

    Add center and main modules

---
+++ center-module/src/main/java/pt/uevora/sd/centermodule/CenterModuleApplication.java
@@ -0,0 +1,13 @@
+package pt.uevora.sd.centermodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenterModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenterModuleApplication.class, args);
+	}
+
+}
---
+++ center-module/src/test/java/pt/uevora/sd/centermodule/CenterModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.uevora.sd.centermodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
