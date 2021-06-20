cd856f931859296f9f8387e1948b0b3267df5ea2
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-06-20 19:29:45 +0100

    CentrosRepository.java: Add findOneByNome queries

--- main-module/src/main/java/pt/uevora/sd/mainmodule/CentrosRepository.java
+++ main-module/src/main/java/pt/uevora/sd/mainmodule/CentrosRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CentrosRepository extends CrudRepository<Centros, Long> {
     Centros findByid(Long ID);
+    Centros findOneByNome(String nome);
 }
