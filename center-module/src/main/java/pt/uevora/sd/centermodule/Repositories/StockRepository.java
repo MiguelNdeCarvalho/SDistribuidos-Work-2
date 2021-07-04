4493c07a69669454b7fbb8860276db79eec72b70
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-04 17:22:20 +0100

    center-module: Repositories: StockRepository: Fix query

--- center-module/src/main/java/pt/uevora/sd/centermodule/Repositories/StockRepository.java
+++ center-module/src/main/java/pt/uevora/sd/centermodule/Repositories/StockRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import pt.uevora.sd.centermodule.Models.Stock;
 
 public interface StockRepository extends CrudRepository<Stock, Long> {
-    Stock findByid(Long ID);
+    Stock findByData(Long ID);
 }
