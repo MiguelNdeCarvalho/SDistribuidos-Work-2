87e7a22cf37a59d1fc67ea2873bd21b721103854
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-04 02:01:26 +0100

    Add client-module

---
+++ client-module/client.py
@@ -0,0 +1,18 @@
+import requests
+response = requests.get(f"http://localhost:8080/api/v1/getCentros")
+
+centros = []
+
+for item in response.json():
+    centros.append(item)
+
+choice = 0
+for centro in centros:
+    print(f"{choice}-> {centro['nome']}")
+    choice += 1
+
+params = { "nome": "Centro de Vacinação do João Pavia Pereira"}
+
+centro = requests.get("http://localhost:8080/api/v1/getCentro/", params=params)
+
+print(centro.json())
