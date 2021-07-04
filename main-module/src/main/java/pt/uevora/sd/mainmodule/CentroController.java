9d92e8f957fd7cee465f14b0ed255fdadff07b80
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-04 01:31:01 +0100

    main-module: CentroController: getCentro requestParam

--- main-module/src/main/java/pt/uevora/sd/mainmodule/CentroController.java
+++ main-module/src/main/java/pt/uevora/sd/mainmodule/CentroController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,9 +38,9 @@ public class CentroController {
     }
 
 	@GetMapping(
-        path = "/getCentro/{nome}",
+        path = "/getCentro",
         produces = "application/json")
-    Centros getCentroByNome(@PathVariable String nome){
+    Centros getCentroByNome(@RequestParam String nome){
         return centrosRepository.findOneByNome(nome);
     }
 
