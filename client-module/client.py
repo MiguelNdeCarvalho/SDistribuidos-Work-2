3e9dffbce1cd5507687f662cbab624c21696c358
jperas243 <joaop.pereira@sapo.pt>
2021-07-06 00:04:33 +0100

    client.py: inicial request

--- client-module/client.py
+++ client-module/client.py
@@ -1,70 +1,46 @@
 import requests
 from datetime import date
 
+option = 0
 
-
-response = requests.get("http://localhost:8000/api/v1/getCentros")
+print("--------Client-Module--------")
 
 centros = []
+response = requests.get("http://localhost:8000/api/v1/getCentros")
 
 for item in response.json():
     centros.append(item)
 
-choice = 0
+choice = 1
 for centro in centros:
     print(f"{choice}-> {centro['nome']}")
     choice += 1
 
-#params = {"nome": "Centro de Vacinação do João Pavia Pereira"}
+choice = int(input("Escolha o centro que se deseja conectar: "))
+print(centros[choice-1]['url'])
 
-#centro = requests.get("http://localhost:8000/api/v1/getCentro", params=params)
+centerURL = centros[choice-1]['url']
 
-#print(centro.json())
-
-#params = {"nome": "Miguel Agostinho","idade": 21, "data": datetime.now(),"email": "jperas243@gmail.com"}
-
-#response = requests.get("http://localhost:8000/api/v1/autoAgendamento",params=params)
-
-option = 0
 while 1:
 
-    print("--------Client---------")
-    print("1) Centros de Vacinação")
-    print("2) Escolher centro")
-    print("3) Auto Agendamento")
-    print("4) Confirmar Agendamento")
-    print("5) Exit")
+    print("-------------------------")
+    print("1) Auto Agendamento")
+    print("2) Confirmar Agendamento")
+    print("3) Exit")
 
     option = input("Option: ")
 
     if option == "1" :
-        print(option)
+        
+        params = {"nome": "Miguel Agostinho","idade": 21, "data": datetime.now(),"email": "jperas243@gmail.com"}            
+        response = requests.get("http://localhost:8000/api/v1/autoAgendamento",params=params)
+        print(response.json())
 
-        response = requests.get("http://localhost:8000/api/v1/getCentros")
-
-        centros = []
-
-        for item in response.json():
-            centros.append(item)
-
-        choice = 0
-        for centro in centros:
-            print(f"{choice}-> {centro['nome']}")
-            choice += 1
-            
     elif option == "2" :
-
-        centerName = input("Nome do Centro: ")
-        params = {"nome": "Centro de Vacinação do João Pavia Pereira"}
-        centro = requests.get("http://localhost:8000/api/v1/getCentro", params=params)
-        print(centro)
-
-    #elif option == 3:
+        centerURL = centros[choice-1]['url']
 
 
-    #elif option == 4:
-
-    elif option == "5" :
+    elif option == "3" :
         break
 
     else: 
