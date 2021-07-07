8c9a46dba3ea03973045a614ac90ca9f73e134d5
jperas243 <joaop.pereira@sapo.pt>
2021-07-08 00:03:55 +0100

    missing sort

--- main-module/src/main/java/pt/uevora/sd/mainmodule/Controllers/MainController.java
+++ main-module/src/main/java/pt/uevora/sd/mainmodule/Controllers/MainController.java
@@ -1,8 +1,6 @@
 package pt.uevora.sd.mainmodule.Controllers;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 
@@ -15,6 +13,10 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +26,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import net.bytebuddy.TypeCache.Sort;
 import pt.uevora.sd.mainmodule.Models.*;
 import pt.uevora.sd.mainmodule.Repositories.*;
 
@@ -81,30 +85,69 @@ public class MainController {
     @GetMapping(
         path = "/fornecerVacinas",
         produces = "application/json")
-    void getFornecerVacinas(@RequestParam String n_vacinas, String data) throws ClientProtocolException, IOException{
+    void getFornecerVacinas(@RequestParam String n_vacinas, String data) throws ClientProtocolException, IOException, ParseException{
         
         List<Centros> centros = (List<Centros>) centrosRepository.findAll();
-        HashMap<String,JSONPObject> association= new HashMap<>();  
-        for (Centros centro : centros) {
-            
+        JSONArray global = new JSONArray();
+        
+        for (Centros centro : centros) {    
             //pedido getAgendamentosByData
             CloseableHttpClient httpClient = HttpClients.createDefault();
-            HttpGet request = new HttpGet("http://localhost:8000/api/v1/getVacinasPorDia");
+            HttpGet request = new HttpGet(centro.getUrl() + "getAgendamentos");
     
             try (CloseableHttpResponse response = httpClient.execute(request)) {
     
                 // Get HttpResponse Status
-                System.out.println(response.getStatusLine().toString());
                 HttpEntity entity = response.getEntity();
-
-    
                 if (entity != null) {
                     // return it as a String
                     String result = EntityUtils.toString(entity);
                     System.out.println(result);
+
+                    JSONArray jsonArr = (JSONArray) new JSONParser().parse( result );
+
+                    for (int i = 0; i < jsonArr.size(); i++) {
+                        JSONObject n =  (JSONObject) jsonArr.get(i);
+                        n.put("centro", centro.getNome());
+                        global.add(n);
+
+                    }
                 }
-    
             }
+        }
+
+        JSONArray jsonArr = new JSONArray(jsonArrStr);
+        JSONArray sortedJsonArray = new JSONArray();
+
+        List<JSONObject> jsonValues = new ArrayList<JSONObject>();
+        for (int i = 0; i < jsonArr.length(); i++) {
+            jsonValues.add(jsonArr.getJSONObject(i));
+        }
+        global.sort( jsonValues, new Comparator<JSONObject>() {
+            //You can change "Name" with "ID" if you want to sort by ID
+            private static final String KEY_NAME = "Name";
+
+            @Override
+            public int compare(JSONObject a, JSONObject b) {
+                String valA = new String();
+                String valB = new String();
+
+                try {
+                    valA = (String) a.get(KEY_NAME);
+                    valB = (String) b.get(KEY_NAME);
+                } 
+                catch (JSONException e) {
+                    //do something
+                }
+
+                return valA.compareTo(valB);
+                //if you want to change the sort order, simply use the following:
+                //return -valA.compareTo(valB);
+            }
+        });
+
+        for (int i = 0; i < jsonArr.length(); i++) {
+            sortedJsonArray.put(jsonValues.get(i));
         }
         
         //merge
