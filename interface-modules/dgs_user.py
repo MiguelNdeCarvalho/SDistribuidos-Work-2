7dee0b86c02fb5f44ccfa9c822cf68441afae08b
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-10 17:12:53 +0100

    interface-modules: dgs_user: Updates by @jperas243

--- interface-modules/dgs_user.py
+++ interface-modules/dgs_user.py
@@ -1,30 +1,11 @@
 import requests
-from datetime import date
 
 def startUI():
     option = 0
 
     print("--------DGS-Module--------")
-
-    centros = []
-    response = requests.get("http://localhost:8000/api/v1/getCentros")
-
-    choice = 1
-    for item in response.json():
-        centros.append(item)
-        print(f"{choice}-> {item['nome']}")
-        choice += 1
-
-    
-
-    choice = int(input("Escolha o centro que se deseja conectar: "))
-    print(centros[choice-1]['url'])
-
-    centerURL = centros[choice-1]['url']
-
     while 1:
 
-        print("-------------------------")
         print("1) Fornecer Vacinas")
         print("2) Relatorio")
         print("3) Exit")
@@ -33,22 +14,24 @@ def startUI():
 
         if option == "1" :
             
-            nVacinas = int(input("Insira o numero de vacinas: "))
+            n_vacinas = int(input("Insira o numero de vacinas: "))
             data = input("Insira a data: ")
+            tipo = input("Insira o tipo da vacina: ")
+
             #tipo de vacina
 
-            params = {"data": data,"nVacinas": nVacinas }            
+            params = {"data":data,"n_vacinas": n_vacinas,"tipo":tipo}            
             response = requests.get("http://localhost:8000/api/v1/fornecerVacinas",params=params)
-            print(response.json())
+            print(response.text)
 
         elif option == "2" :
             
-            data = int((input("Insira o dia: ")))
-            requestUrl = "http://localhost:8000/api/v1/relatorio"
+            data = input("Insira o dia: ")
+            requestUrl = "http://localhost:8000/api/v1/nTotalVacinas"
             params = {"data": data}            
             response = requests.get(requestUrl,params=params)
             
-            print(response.text)
+            print(response.json())
 
 
         elif option == "3" :
