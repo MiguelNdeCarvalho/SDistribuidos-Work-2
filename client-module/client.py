9e6857652d519f0eeffa45b2689bed11738cf1e0
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-11 23:41:59 +0100

    client-module: Merge all into one

--- client-module/client.py
+++ client-module/client.py
@@ -1,8 +1,6 @@
-import datetime
 import requests
-import dateutil.parser as timeparser
 
-def startUI():
+def cliente():
     option = 0
 
     print("--------Client-Module--------")
@@ -76,6 +74,104 @@ def startUI():
         else: 
             print("Opção Invalida")
 
+def centro():
+    option = 0
+
+    print("--------Center-Module--------")
+
+    centros = []
+    response = requests.get("http://localhost:8000/api/v1/getCentros")
+
+    choice = 1
+    for item in response.json():
+        centros.append(item)
+        print(f"{choice}-> {item['nome']}")
+        choice += 1
+
+    
+
+    choice = int(input("Escolha o centro que se deseja conectar: "))
+    print(centros[choice-1]['url'])
+
+    centerURL = centros[choice-1]['url']
+
+    while 1:
+
+        print("-------------------------")
+        print("1) Lista de Agendamentos")
+        print("2) Stock de vacinas")
+        print("3) Vacinação")
+        print("4) Exit")
+
+
+        option = input("Option: ")
+
+        if option == "1" :
+            response = requests.get(centerURL+"getAgendamentos",params=params)
+            print(response.json())
+
+        elif option == "2" :
+            
+            response = requests.get(centerURL+"getStocks",params=params)
+            print(response.json())
+            
+            print(response.text)
+
+        elif option == "3" :
+            cc = int((input("Insira o numero do cartão de cidadão: ")))
+            tipo = input("Insira o tipo da vacina: ")
+
+            #tipo de vacina
+
+            params = {"tipoVacina": tipo}            
+            response = requests.post(f"{centerURL}setVacinado/{cc}",params=params)
+            print(response.text)
+
+        elif option == "4" :
+            break
+
+        else: 
+            print("Opção Invalida")
+
+def dgs():
+    option = 0
+
+    print("--------DGS-Module--------")
+    while 1:
+
+        print("1) Fornecer Vacinas")
+        print("2) Relatorio")
+        print("3) Exit")
+
+        option = input("Option: ")
+
+        if option == "1" :
+            
+            n_vacinas = int(input("Insira o numero de vacinas: "))
+            data = input("Insira a data: ")
+            tipo = input("Insira o tipo da vacina: ")
+
+            #tipo de vacina
+
+            params = {"data":data,"n_vacinas": n_vacinas,"tipo":tipo}            
+            response = requests.get("http://localhost:8000/api/v1/fornecerVacinas",params=params)
+            print(response.text)
+
+        elif option == "2" :
+            
+            data = input("Insira o dia: ")
+            requestUrl = "http://localhost:8000/api/v1/nTotalVacinas"
+            params = {"data": data}            
+            response = requests.get(requestUrl,params=params)
+            
+            print(response.json())
+
+
+        elif option == "3" :
+            break
+
+        else: 
+            print("Opção Invalida")
 
 if __name__ == '__main__':
-    startUI()
+    cliente()
